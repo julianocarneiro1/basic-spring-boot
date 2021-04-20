ecb6809ace8bf672e8c3e51a906c3ecc5469acf6
Juliano Carneiro <julianoomc@gmail.com>
2021-04-20 16:12:15 -0300

    Product in memory

--- src/main/java/com/julianocarneiro/myfirstproject/entities/Product.java
+++ src/main/java/com/julianocarneiro/myfirstproject/entities/Product.java
@@ -1,27 +1,26 @@
 package com.julianocarneiro.myfirstproject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable {
+public class Product implements Serializable {	
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
+	private Double price;
 	
-	private List<Product> products = new ArrayList<>();
+	private Category category;
 	
-	public Category() {
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -40,17 +39,28 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-	}
-
-	@JsonIgnore
-	public List<Product> getProducts() {
-		return products;
 	}
 
 	@Override
@@ -61,7 +71,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -69,6 +79,7 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	
 	
 }
